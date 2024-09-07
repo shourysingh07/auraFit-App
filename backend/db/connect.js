b26ffabe803f391652dca913029f6a7013c5ef5b
shourysingh07 <shourysingh007@gmail.com>
2024-09-07 23:33:08 +0530

    backend changes

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const connectDB=(URL)=>{
-    return mongoose.connect("mongodb+srv://aryanjaiswal0602:VcAJ25Xn0bLHivLX@cluster0.cpkjkbk.mongodb.net/")
+    return mongoose.connect(process.env.MONGO_URI);
 }
 
 module.exports=connectDB
